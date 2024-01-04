3b4010c9c69160e77e8b9a8ca94770314ea4c41d
zDeVolTz <vlad.pinti@gmail.com>
2024-01-04 23:56:42 +0200

    add new variant

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-var codes = {
+ codes = {
   "6\n": "Польща",
   "38\n": "Україна",
   "1\n": "США"
 };
 
-var arr = Object.entries(codes).map(([key, value]) => [key.replace(/\n/g, ''), value]);
+let arr = Object.entries(codes).map(([key, value]) => [key.replace(/\n/g, ''), value]);
 
 arr.forEach(([key, value]) => console.log(`${key} : ${value}`));
+
+
+//Такой метод тоже работает добавление проебела перед ключем но всеравно как то грустно то что у меня получается , 
+// не понимю как не лезть в исходный обьект к ключам
+codes2 = {
+  " 6": "Польща",
+  " 38": "Україна",
+  " 1": "США"
+};
+
+let arr2 = Object.entries(codes2).map(([key, value]) => [key.trim(), value]);
+
+arr2.forEach(([key, value]) => console.log(`${key} : ${value}`));
